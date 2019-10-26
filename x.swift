27c92be374a3e25211cc626221fa7395217b6118
Dave Abrahams <dabrahams@apple.com>
2019-10-26 10:29:54 -0700

    change 1

--- x.swift
+++ x.swift
@@ -1,5 +1,6 @@
                  let z = someFunc(
                      max(a / b, 0), in: c, by: d)
-                 
+
+                 print("some message")
                  otherFunc(
                      &q[m], arg: n)
